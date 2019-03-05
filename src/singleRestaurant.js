82e6921cd9044ec4f6cba0e620877b781f2f95ae
Kevin Nabeth <kgnabeth@gmail.com>
2019-03-05 12:22:53 +0100

    first component

---
+++ src/singleRestaurant.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class SingleRestaurant extends Component {
+  render() {
+    return (
+      <div className="single-restaurant">
+        RESTO
+      </div>
+    );
+  }
+}
+
+export default SingleRestaurant;
