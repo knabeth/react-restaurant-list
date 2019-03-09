7c209dc18f63004a7728e529bdf0421ce8cced89
Cédrick Le Guen <cle-guen@MacBook-Pro-de-Cedrick.local>
2019-03-09 19:34:32 +0100

    login_form

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -2,17 +2,35 @@ import React, { Component } from 'react';
 import "./css/LoginForm.css";
 
 class LoginForm extends Component {
-logIn() {
-	alert("login")
-}
+	state = {
+		username: "",
+		password: ""
+	}
+	logIn(e) {
+		e.preventDefault()
+	}
+	usernameChange(e) {
+		this.setState({username: e.target.value})
+		console.log(this.state.username)
+	}
 	render() {
-
 		return (
 			<div className="login_form_container">
-				<form className="login_form" onSubmit={()=> {this.logIn()}}>
-					<input type="text" name="username" placeholder="Identifiant" />
-					<input type="password" name="password" placeholder="Mot de passe" />
-					<input type="submit" value="Valider" />
+				<form className="login_form" onSubmit={(e) => { this.logIn(e)}}>
+					<input
+						type="text"
+						name="username"
+						placeholder="Identifiant"
+						value={this.state.username}
+						onChange={(e) => {this.usernameChange(e)}}/>
+					<input
+						type="password"
+						name="password"
+						placeholder="Mot de passe"
+						value={this.state.password}/>
+					<input
+						type="submit"
+						value="Valider"/>
 				</form>
 			</div>
 		);
